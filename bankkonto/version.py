26bd4a0cbdb8c0e4ddc0bc0ec64f35f66e2a7b97
Henrik Blidh <henrik.blidh@swedwise.com>
2017-03-20 13:49:39 +0100

    Updated import statements.

--- bankkonto/version.py
+++ bankkonto/version.py
@@ -13,5 +13,5 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 version = __version__  # backwards compatibility name
